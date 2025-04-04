88cea30908bcc4bccb2203a2abb85702c95bf2c7
github_username <mfernandezlakatos@gmail.com>
2025-04-04 18:57:11 +0200

    cosas de bat

---
+++ pruebaQuery.bat
@@ -0,0 +1,2 @@
+curl -X GET http://172.16.240.27:2304/v2/entities?id=Mote1 ^
+  -H "Accept: application/json"
---
+++ pruebaUpdate.bat
@@ -0,0 +1,3 @@
+curl -X PATCH http://172.16.240.27:2304/v2/entities/Mote1/attrs ^
+  -H "Content-Type: application/json" ^
+  -d "@jsonpruebaUpdate.txt"

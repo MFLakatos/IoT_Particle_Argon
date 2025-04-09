b0c15e6b63fab5ad186a392e00193ac0ce566895
github_username <mfernandezlakatos@gmail.com>
2025-04-09 18:13:18 +0200

    Final 9-4-25

--- pruebaUpdate.bat
+++ pruebaUpdate.bat
@@ -1,3 +1,1 @@
-curl -X PATCH http://172.16.240.27:2304/v2/entities/Mote1/attrs ^
-  -H "Content-Type: application/json" ^
-  -d "@jsonpruebaUpdate.txt"
+curl 172.16.240.27:2304/v2/entities/Mote2/attrs -s -S --header "Content-Type: application/json" -X PATCH -d @jsonpruebaUpdate.txt
